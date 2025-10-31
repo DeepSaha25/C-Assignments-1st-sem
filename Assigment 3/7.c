381a2b38945e6869f5af87245c5e5b56fbc3fd7d
AbiroyKarmakar21 <karmakarabiroy@gmail.com>
2025-10-31 21:39:50 +0530

    Refactor program to accept start and end range

--- Assigment 3/7.c
+++ Assigment 3/7.c
@@ -1,16 +1,18 @@
-//7.Write a C program to print all numbers in a given range which are divisible by 3 or 5 using do-while
+//7.Write a C program to print all numbers in a given range which are divisible by 3 or 5 using do-while loop
 
 #include <stdio.h>
-void main(){
-    int i,n;
-    printf("enter range of numbers to check and print whether divisible by 3 or 5 :");
-    scanf("%d",&n);
 
-    i=0;
+void main(){
+    int start, end;
+    printf("Enter the start and end range:");
+    scanf("%d %d", &start, &end);
+    int i= start;
     do{
         if(i%3==0 || i%5==0)
-            printf("%d \n",i);
+        printf("%d \n",i);
         i++;
     }
-    while(i<=n);
+    while(i<=end);
+   
+    
 }
