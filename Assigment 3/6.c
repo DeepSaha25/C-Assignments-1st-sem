b29402b0111d80cb8b6a5fee7061121b72438c77
DeepSaha25 <ideepsaha25@gmail.com>
2025-10-31 18:53:04 +0530

    solved q6

--- Assigment 3/6.c
+++ Assigment 3/6.c
@@ -1,1 +1,15 @@
-//
+//6.Write a C program to print all numbers in a given range which are divisible by 3 or 5 using while loop.
+
+#include <stdio.h>
+void main(){
+    int i,n;
+    printf("enter range of numbers to check and print whether divisible by 3 or 5 :");
+    scanf("%d",&n);
+    i=0;
+    while(i<=n){
+       if(i%3==0 || i%5==0){
+        printf("%d \n",i);
+       }
+       i++;
+    }
+}
