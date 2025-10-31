dbc7765852ad065a76505d50e8d7107a28c67041
AbiroyKarmakar21 <karmakarabiroy@gmail.com>
2025-10-31 13:00:13 +0530

    Implement n natural numbers printing program
    Add a program to print the first n natural numbers.

--- Assigment 3/2.c
+++ Assigment 3/2.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+int main(){
+  int n,i=1;
+printf("Enter the value of n:");
+scanf("%d", &n);
+printf("The first %d natural numbers are :\n",n);
+while(i<=n){
+printf("%d \n",i);
+i++;
+}
+return 0;
+}
+
