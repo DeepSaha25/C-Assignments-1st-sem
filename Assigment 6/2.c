b2278422657f819630c6ef4d0ee9649c96912213
DeepSaha25 <ideepsaha25@gmail.com>
2025-11-16 12:08:32 +0530

    2.c

---
+++ Assigment 6/2.c
@@ -0,0 +1,22 @@
+// 1. Write a C program to calculate the sum and average of all elements in a 1D array.
+
+#include <stdio.h>
+void main(){
+    int a[10],n,i,s;
+    printf("Enter the no of elements in array ");
+    scanf(" %d",&n);
+    for(i=0;i<n;i++){
+        printf("Enter value for a[%d]",i);
+        scanf("%d",&a[i]);
+    }
+    
+    s=0;
+    for(i=0;i<n;i++){
+        s=s+a[i];
+    }
+    printf("The Sum is %d\t",s);
+
+    float avg = (float)s / n;
+     printf("\nThe Average is %f",avg);    
+    printf("\n");
+}
